43ea254321679f69ff3087014d24b3b4406ed1ff
sweetlilmre <sweetlilmre@gmail.com>
2014-07-17 22:14:45 +0200

    fixed some strings

--- memstrings.c
+++ memstrings.c
@@ -1,7 +1,7 @@
 #include <avr/pgmspace.h>
 #include "memstrings.h"
 
-prog_char S_INIT[] PROGMEM = "Initialising";
+prog_char S_INIT[] PROGMEM = "Init...";
 prog_char S_INIT_FAILED[] PROGMEM = "Init failed!";
 prog_char S_INIT_OK[] PROGMEM = "Init OK.";
 prog_char S_NO_FILES_FOUND[] PROGMEM = "No files found!";
@@ -9,10 +9,9 @@ prog_char S_SELECT_FILE[] PROGMEM = "Select file:";
 prog_char S_READ_FAILED[] PROGMEM = "Read failed";
 prog_char S_OPEN_FAILED[] PROGMEM = "Open failed";
 prog_char S_INVALID_TAP[] PROGMEM = "Invalid TAP!";
-prog_char S_LOADING[] PROGMEM = "Loading!";
+prog_char S_LOADING[] PROGMEM = "Loading:";
 prog_char S_LOADING_COMPLETE[] PROGMEM = "Complete!";
 prog_char S_LOADING_ABORTED[] PROGMEM = "Aborted!";
 prog_char S_DIRECTORY_ERROR[] PROGMEM = "CHDIR ERROR!";
 prog_char S_MAX_BLANK_LINE[] PROGMEM = "                    ";
 prog_char S_TAP_MAGIC_C64[] PROGMEM = "C64-TAPE-RAW";
-prog_char S_UP_A_DIR[] PROGMEM = "..";
